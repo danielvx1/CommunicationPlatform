5ce7291bac8152c7fde9b0e6a2abb0bd21a8d10d
danielvx1 <dtv157@gmail.com>
2025-03-04 14:53:32 -0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Text } from '@mantine/core'
+
+function App() {
+  
+
+  return <Text fw={700}>Hello World!</Text>
+  
+}
+
+export default App
