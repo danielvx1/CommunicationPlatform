0aa2396157993754504337b9583243729bcdff75
danielvx1 <dtv157@gmail.com>
2025-03-10 23:24:57 -0500

    Added navbar, not centered

--- src/components/navigation/Sidebar.tsx
+++ src/components/navigation/Sidebar.tsx
@@ -1,11 +1,50 @@
 import { UserButton } from '@clerk/clerk-react'
 import React from 'react'
+import classes from './Sidebar.module.css'
+import { Button, Center, Stack, useMantineColorScheme } from '@mantine/core'
+import { IconArrowsJoin, IconMoon, IconPlus, IconSun } from '@tabler/icons-react'
 
 function Sidebar() {
+  const { colorScheme, toggleColorScheme } = useMantineColorScheme()
+
   return (
-    <div>Sidebar
-        <UserButton />
-    </div>
+    <nav className={colorScheme === 'dark' ? 'navbar-dark' : 'navbar-light'}>
+      <div className='nav-items'>
+        <Center>
+          <Button
+            className={classes.link}
+            variant='subtle'
+            radius={100}
+            onClick={() => { }}
+          >
+            <IconPlus radius={100} />
+          </Button>
+        </Center>
+
+        <Center>
+          <Button
+            className={classes.link}
+            variant='subtle'
+            radius={100}
+            onClick={() => { }}
+          >
+            <IconArrowsJoin radius={100} />
+          </Button>
+        </Center>
+
+        <Stack justify='center' align='center'>
+          <Button
+            className={classes.link}
+            variant='subtle'
+            onClick={toggleColorScheme}
+            radius={100}
+            p={0}
+          >
+            {colorScheme == 'dark' ? (<IconMoon radius={100} />) : (<IconSun radius={100} />)}
+          </Button>
+        </Stack>
+      </div>
+    </nav>
   )
 }
 
